ccc53cbb297f8ae6dc27f3549598a0ee37eddda8
loi3009 <minhloi.3009@gmail.com>
2018-04-18 16:41:57 +0700

    Shop My Pham

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/font-awesome.min.css" />
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/style.css">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/magnific-popup.css">
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.css">
+  <?php wp_head(); ?>
+</head>
+<body>
+	<div class="wrapper">
+		<header id="header">
+			<div class="header-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6">
+							<ul class="header-top-left">
+								<li>Gọi đặt hàng ngay: 01658949680</li>
+							</ul>
+						</div>
+						<div class="col-sm-6">
+							<ul class="header-top-right text-right">
+								<li class="account"><a href="<?php bloginfo('url'); ?>/tai-khoan">Tài khoản</a></li>
+                                <li class="sitemap"><a href="<?php bloginfo('url'); ?>/lien-he">Liên hệ</a></li>
+                                <li class="cart"><a href="<?php bloginfo('url'); ?>/gio-hang">Giỏ hàng</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="header">
+				<div class="container">
+					<nav class="navbar">
+						<div class="navbar-header mtb_20">
+							<a class="navbar-brand"> href="<?php bloginfo( 'url' ) ?>">
+								<img src="<?php bloginfo( 'template_directory' ) ?>/images/logo.png" style="width: 190px;">
+							</a>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+	</div>
+</body>
+</html>
